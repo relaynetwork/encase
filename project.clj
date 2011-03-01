111c32f59d1bc3f1f7700aba62dd9e46ad8a30d7
Kyle Burton <kburton@relaynetwork.com>
2011-03-01 17:30:36 -0500

    namespace project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject encase "1.0.0"
+(defproject com.relaynetwork/encase "1.0.0"
   :description "Command Pattern Library for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
