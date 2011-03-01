67bbe6f3fe5d3834907bd38457807ba177888311
Kyle Burton <kburton@relaynetwork.com>
2011-03-01 17:26:55 -0500

    initial lein generated project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject encase "1.0.0-SNAPSHOT"
+  :description "Command Pattern Library for Clojure"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/encase/test/core.clj
@@ -0,0 +1,6 @@
+(ns encase.test.core
+  (:use [encase.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
