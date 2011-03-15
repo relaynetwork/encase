b659ecb7224e1625b334732d07cf51f02e6eb3d2
Kyle Burton <kburton@relaynetwork.com>
2011-03-15 14:15:02 -0400

    bump to 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/encase "1.0.1-SNAPSHOT"
+(defproject com.relaynetwork/encase "1.0.1"
   :description "Command Pattern Library for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
