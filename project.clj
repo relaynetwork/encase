547e9f44bd8106363dad5fd26593b1a10c0036bd
Kyle Burton <kburton@relaynetwork.com>
2011-06-01 09:57:18 -0400

    use 1.0.33 of clj-etl-utils

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Command Pattern Library for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.kyleburton/clj-etl-utils "1.0.25"]]
+                 [org.clojars.kyleburton/clj-etl-utils "1.0.33"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
